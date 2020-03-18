d20f264b5af7498fbbaede5d0174e9886e1030a5
madmanmoon93 <62351204+madmanmoon93@users.noreply.github.com>
2020-03-18 20:56:29 +0200

    Update SieveofEratosthenes.py

--- SieveofEratosthenes.py
+++ SieveofEratosthenes.py
@@ -1,3 +1,7 @@
+# Iteratively marking as composite (i.e., not prime) the multiples of each prime, starting with the first prime number, 2. 
+# The multiples of a given prime are generated as a sequence of numbers starting from that prime
+# with constant difference between them that is equal to that prime.
+
 n=100
 list=list(range(2,n+1))
 for divisor in list:
